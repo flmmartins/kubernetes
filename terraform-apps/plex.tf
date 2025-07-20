42970a424d46f84b976b2c52ddf91146a5367ad9
Fernanda Martins <flmmmartins@gmail.com>
2025-07-20 16:29:57 +0200

    Add plex on ingress and public domain

--- terraform-apps/plex.tf
+++ terraform-apps/plex.tf
@@ -1,7 +1,6 @@
 locals {
-  plex_app_name      = "plex"
-  plex_url           = "${local.plex_app_name}.${var.apps_domain}"
-  plex_ip            = cidrhost(var.plex_ip_cidr, 0)
+  plex_app_name = "plex"
+  plex_url      = "${local.plex_app_name}.${var.public_domain}"
   plex_common_labels = {
     "part-of" = "media-server"
   }
@@ -10,38 +9,6 @@ locals {
 resource "kubernetes_namespace_v1" "plex" {
   metadata {
     name = local.plex_app_name
-  }
-}
-
-#TV and mobile apps don't work using reverse proxy. Check readme
-resource "kubernetes_manifest" "plex-ip-address-pool" {
-  manifest = {
-    apiVersion = "metallb.io/v1beta1"
-    kind       = "IPAddressPool"
-    metadata = {
-      name      = local.plex_app_name
-      namespace = "metallb"
-      labels    = local.plex_common_labels
-    }
-    spec = {
-      addresses  = [var.plex_ip_cidr]
-      autoAssign = false
-    }
-  }
-}
-
-resource "kubernetes_manifest" "plex-l2-advertisement" {
-  manifest = {
-    apiVersion = "metallb.io/v1beta1"
-    kind       = "L2Advertisement"
-    metadata = {
-      name      = local.plex_app_name
-      namespace = "metallb"
-      labels    = local.plex_common_labels
-    }
-    spec = {
-      ipAddressPools = [kubernetes_manifest.plex-ip-address-pool.manifest.metadata.name]
-    }
   }
 }
 
@@ -115,58 +82,53 @@ resource "helm_release" "plex" {
   version    = var.plex_chart_version
   chart      = "plex-media-server"
   values = [
-  <<-EOF
-  pms:
-    # On first boot: https://www.plex.tv/claim/. Edit env var below - claim token is temp
-    storageClassName: persistent
-    configStorage: 5Gi
-    resources:
-      requests:
-        cpu: 100m
-        memory: 200Mi
-  commonLabels: ${jsonencode(merge(local.plex_common_labels, { "component" = "plex" }))}
-  affinity:
-    podAntiAffinity:
-        preferredDuringSchedulingIgnoredDuringExecution:
-        - weight: 100
-          podAffinityTerm:
-            labelSelector:
-              matchExpressions:
-              - key: component
-                operator: In
-                values:
-                - plex
-            topologyKey: kubernetes.io/hostname
-  extraEnv:
-    HOSTNAME: "TalosPlexServer"
-    TZ: "Europe/Amsterdam"
-    ALLOWED_NETWORKS: "0.0.0.0/0"
-    ADVERTISE_IP: "http://${local.plex_ip}:32400,https://${local.plex_ip}:32400"
-    #PLEX_CLAIM:
-  extraVolumes:
-  - name: movies
-    persistentVolumeClaim:
-      claimName: ${kubernetes_persistent_volume_claim_v1.plex_movies.metadata[0].name}
-  - name: music
-    persistentVolumeClaim:
-      claimName: ${kubernetes_persistent_volume_claim_v1.plex_music.metadata[0].name}
-  - name: tvshows
-    persistentVolumeClaim:
-      claimName: ${kubernetes_persistent_volume_claim_v1.plex_tvshows.metadata[0].name}
-  extraVolumeMounts:
-  - name: movies
-    mountPath: /movies
-    readOnly: true
-  - name: music
-    mountPath: /music
-    readOnly: true
-  - name: tvshows
-    mountPath: /tv_shows
-    readOnly: true
-  service:
-    annotations:
-      "metallb.universe.tf/address-pool": ${kubernetes_manifest.plex-l2-advertisement.manifest.metadata.name}
-    type: LoadBalancer
-  EOF
+    <<-EOF
+    pms:
+      # On first boot: https://www.plex.tv/claim/. Edit env var below - claim token is temp
+      storageClassName: persistent
+      configStorage: 5Gi
+      resources:
+        requests:
+          cpu: 100m
+          memory: 200Mi
+    commonLabels: ${jsonencode(merge(local.plex_common_labels, { "component" = "plex" }))}
+    extraEnv:
+      HOSTNAME: "TalosPlexServer"
+      TZ: "Europe/Amsterdam"
+      ALLOWED_NETWORKS: "0.0.0.0/0"
+      ADVERTISE_IP: "https://${local.plex_url}"
+      #PLEX_CLAIM:
+    extraVolumes:
+    - name: movies
+      persistentVolumeClaim:
+        claimName: ${kubernetes_persistent_volume_claim_v1.plex_movies.metadata[0].name}
+    - name: music
+      persistentVolumeClaim:
+        claimName: ${kubernetes_persistent_volume_claim_v1.plex_music.metadata[0].name}
+    - name: tvshows
+      persistentVolumeClaim:
+        claimName: ${kubernetes_persistent_volume_claim_v1.plex_tvshows.metadata[0].name}
+    extraVolumeMounts:
+    - name: movies
+      mountPath: /movies
+      readOnly: true
+    - name: music
+      mountPath: /music
+      readOnly: true
+    - name: tvshows
+      mountPath: /tv_shows
+      readOnly: true
+    ingress:
+      enabled: true
+      ingressClassName: "nginx"
+      url: ${local.plex_url}
+      annotations: 
+        kubernetes.io/tls-acme: "true" #Auto-tls creation by cert-manager
+        cert-manager.io/common-name: "${local.plex_url}"
+      tls:
+      - hosts:
+        - ${local.plex_url}
+        secretName: ${local.plex_app_name}-tls
+    EOF
   ]
 }
