cbb38c2a45beaa8c247db7325808fe33fe2e8fd9
Fernanda Martins <flmmmartins@gmail.com>
2026-02-18 00:02:43 +0100

    Don't add manifest to terraform in helm provider due to mutating hooks

--- terraform-base/providers.tf
+++ terraform-base/providers.tf
@@ -2,11 +2,9 @@ provider "kubernetes" {
   host = var.kubernetes_api
 }
 
-provider "helm" {
-  experiments = {
-    manifest = true
-  }
-}
+# Don't use manifests = true in helm provider. The mutating webhook from vault injects things in vault pods
+# Terraform wants to remove then and you end up in a loop
+provider "helm" {}
 
 provider "vault" {
   address = var.vault_address
